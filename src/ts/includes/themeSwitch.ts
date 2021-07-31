7288fb8655e6d3961aeec1adc7f72a804e442174
Robin van der Linde <robin.vanderlinde@gmail.com>
2021-07-31 12:29:55 +0200

    💄Fix styling for other pages and use localStorage

--- src/ts/includes/themeSwitch.ts
+++ src/ts/includes/themeSwitch.ts
@@ -5,20 +5,29 @@ const themeSwitch = document.querySelector("#themeSwitch") as HTMLInputElement;
 export let theme!: string;
 
 function changeTheme() {
-  document.documentElement.setAttribute("data-theme", theme);
+  document.documentElement.setAttribute(
+    "data-theme",
+    localStorage.getItem("theme")
+  );
 
   if (typeof themeSwitch?.checked !== "undefined") {
     themeSwitch.checked = theme === "dark";
+    localStorage.setItem("theme", theme);
   }
 }
 
 export default function initTheme(): void {
   // Initialize theme
-  theme =
+  if (localStorage.getItem("theme")) {
+    theme = localStorage.getItem("theme");
+  } else if (
     window.matchMedia("(prefers-color-scheme: dark)").matches === true
-      ? "dark"
-      : "light";
-
+  ) {
+    theme = "dark";
+    localStorage.setItem("theme", theme);
+  } else {
+    theme = "light";
+  }
   changeTheme();
 
   // Update theme when browser configuration changes
@@ -26,6 +35,7 @@ export default function initTheme(): void {
     .matchMedia("(prefers-color-scheme: dark)")
     .addEventListener("change", (event: MediaQueryListEvent) => {
       theme = event.matches === true ? "dark" : "light";
+      localStorage.setItem("theme", theme);
 
       changeTheme();
     });
@@ -33,6 +43,7 @@ export default function initTheme(): void {
   // Update theme when user click on them switch
   themeSwitch?.addEventListener("click", (event: MouseEvent) => {
     theme = themeSwitch.checked === true ? "dark" : "light";
+    localStorage.setItem("theme", theme);
 
     changeTheme();
   });
