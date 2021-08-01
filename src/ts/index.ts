ea7babdbc67ead518a69d1504d4607778047e6a8
Robin van der Linde <robin.vanderlinde@gmail.com>
2021-08-01 22:58:26 +0200

    🔥 Remove tv widget

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,29 +1,15 @@
 import "bootstrap/js/dist/collapse";
 
-import { tvWidget } from "./includes/tvWidget";
 import { osmWidget } from "./includes/osmWidget";
 import { githubWidget } from "./includes/githubWidget";
 import initTheme from "./includes/themeSwitch";
 
 initTheme();
 
-const tvWidgetDiv = document.getElementById("tvWidget");
-const traktUser = "robinlin";
-const traktApiKey =
-  "19a5ec4d8dabf24914fd8436a28b759633226c296763768126bb21940a477ab0";
-const tmdbApiKey = "cd059bc717a900bf749969433ec08789";
-
-updateWidget();
-var timer = setInterval(updateWidget, 30000);
-
-function updateWidget() {
-  tvWidget(tvWidgetDiv, traktUser, traktApiKey, tmdbApiKey);
-}
-
 const osmWidgetDiv = document.getElementById("osmChangesets");
 const osmUser = "Robin van der Linde";
-osmWidget(osmWidgetDiv, osmUser, 20);
+osmWidget(osmWidgetDiv, osmUser, 5);
 
 const githubWidgetDiv = document.getElementById("githubActivity");
 const githubUser = "robinlinde";
-githubWidget(githubWidgetDiv, githubUser, 15);
+githubWidget(githubWidgetDiv, githubUser, 5);
