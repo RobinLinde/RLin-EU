17885cb7554434f81ce19f6a03668cb6a91b2c33
Robin van der Linde <robin.vanderlinde@gmail.com>
2021-08-01 23:38:17 +0200

    ✨ Steam widget

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,10 +1,16 @@
 import "bootstrap/js/dist/collapse";
 
+import { steamWidget } from "./includes/steamWidget";
 import { osmWidget } from "./includes/osmWidget";
 import { githubWidget } from "./includes/githubWidget";
 import initTheme from "./includes/themeSwitch";
 
 initTheme();
+
+const steamWidgetDiv = document.getElementById("steamActivity");
+const steamKey = "25F0C2FF68F43EFEFE4DCAC9C8E4F36";
+const steamUser = "76561198136326705";
+steamWidget(steamWidgetDiv, steamUser, steamKey);
 
 const osmWidgetDiv = document.getElementById("osmChangesets");
 const osmUser = "Robin van der Linde";
