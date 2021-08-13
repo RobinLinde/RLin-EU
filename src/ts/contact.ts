395d3bc11a521b14d00cab139716e9ea7000a4d8
Robin van der Linde <robin.vanderlinde@gmail.com>
2021-08-13 22:23:47 +0200

    🚚 Change endpoint

--- src/ts/contact.ts
+++ src/ts/contact.ts
@@ -7,6 +7,7 @@ initTheme();
 const steamBadge = document.getElementById("steamBadge") as HTMLSpanElement;
 const steamKey = "25F0C2FF68F43EFEFE4DCAC9C8E4F36";
 const steamUser = "76561198136326705";
+const corsEndpoint = "https://cors.rlin.eu";
 
-const steamApi = new Steam(steamUser, steamKey);
+const steamApi = new Steam(steamUser, steamKey, corsEndpoint);
 steamApi.steamBadge(steamBadge);
